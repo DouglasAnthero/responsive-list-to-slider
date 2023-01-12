a5d3974f8013f0d16bd2b68900accbe905a66215
DouglasAnthero <douglasdemello@gmail.com>
2023-01-12 17:12:24 -0300

    First commit

---
+++ main.js
@@ -0,0 +1,48 @@
+const slides = document.querySelectorAll('.slide');
+const next = document.querySelector('#next');
+const prev = document.querySelector('#prev');
+const auto = false;
+const intervalTime = 500;
+let slideInterval;
+
+
+const nextSlide = () => {
+    const current = document.querySelector('.current');
+    current.classList.remove('current');
+    if (current.nextElementSibling) {
+        current.nextElementSibling.classList.add('current');
+    } else {
+        slides[0].classList.add('current');
+    }
+    setTimeout(() => current.classList.remove('current'));
+}
+
+const prevSlide = () => {
+    const current = document.querySelector('.current');
+    current.classList.remove('current');
+    if (current.previousElementSibling) {
+        current.previousElementSibling.classList.add('current');
+    } else {
+        slides[slides.length - 1].classList.add('current');
+    }
+    setTimeout(() => current.classList.remove('current'));
+}
+
+next.addEventListener('click', (event) => {
+    nextSlide();
+    if (auto) {
+        clearInterval(slideInterval);
+        slideInterval = setInterval(nextSlide, intervalTime);
+    };
+});
+prev.addEventListener('click', (event) => {
+    prevSlide();
+    if (auto) {
+        clearInterval(slideInterval);
+        slideInterval = setInterval(nextSlide, intervalTime);
+    };
+});
+
+if (auto) {
+    slideInterval = setInterval(nextSlide, intervalTime);
+}
